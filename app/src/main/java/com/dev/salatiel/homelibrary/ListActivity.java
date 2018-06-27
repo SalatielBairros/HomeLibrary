43aaa5f2392848f208b912079ca0708cd0caf85f
Salatiel Bairros <salatiel.costabairros@gmail.com>
2018-06-27 01:12:11 -0300

    1 - Inclusão do menu lateral 2 - Atualização da Toolbar 3 - Correções no CRUD Livros

--- app/src/main/java/com/dev/salatiel/homelibrary/ListActivity.java
+++ app/src/main/java/com/dev/salatiel/homelibrary/ListActivity.java
@@ -2,10 +2,17 @@ package com.dev.salatiel.homelibrary;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -17,11 +24,25 @@ public class ListActivity extends AppCompatActivity {
     private ListView lista;
     FloatingActionButton btnAdd;
     LivroController livroController;
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    ActionBarDrawerToggle mDrawerToggle;
+    private NavigationView nvDrawer;
+    private final static String TAG = "LIST";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        toolbar = findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        mDrawer = findViewById(R.id.drawer_layout);
+        nvDrawer = findViewById(R.id.navigation);
+        //getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // Setup drawer view
+        //setupDrawerContent(nvDrawer);
 
         livroController = new LivroController(this);
         btnAdd = findViewById(R.id.btnAdd);
@@ -33,12 +54,39 @@ public class ListActivity extends AppCompatActivity {
         });
 
         carregarLista();
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawer, R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public boolean onOptionsItemSelected(MenuItem item) {
+                return super.onOptionsItemSelected(item);
+            }
+        };
+
+        nvDrawer.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                mDrawer.closeDrawers();  // CLOSE DRAWER
+                return true;
+            }
+        });
+
+        mDrawer.addDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
     }
 
     @Override
     public void onRestart() {
         carregarLista();
         super.onRestart();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void carregarLista(){
